583dd6786a8701fce50c312fbbac839bfbf5be92
YoneyamaShunpei <yoneyamashunpei@gmail.com>
2020-02-22 01:15:33 +0900

    add:toolbar and backbotton

--- app/src/main/java/com/komeyama/sample/design/material/ui/bottombarfragment/BottomBarFragment.kt
+++ app/src/main/java/com/komeyama/sample/design/material/ui/bottombarfragment/BottomBarFragment.kt
@@ -1,14 +1,12 @@
 package com.komeyama.sample.design.material.ui.bottombarfragment
 
-import android.content.res.ColorStateList
-import android.graphics.Color
-import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import android.view.*
 import android.widget.TextView
 import androidx.annotation.LayoutRes
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomappbar.BottomAppBar
@@ -126,6 +124,10 @@ class BottomBarFragment : Fragment() {
                 background.setTint(ContextCompat.getColor(activity!!, R.color.colorPrimaryDark))
                 bottom_bar_fab_position_change_animation.text = getString(R.string.bottom_bar_fab_position_animation_button_scale)
             }
+        }
+
+        bottom_fragment_top_toolbar.setNavigationOnClickListener {
+            findNavController().navigateUp()
         }
     }
 }
