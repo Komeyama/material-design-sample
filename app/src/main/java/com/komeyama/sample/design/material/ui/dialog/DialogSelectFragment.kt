6599e3c8c1d502603ac0e91442d7e92a805f5321
YoneyamaShunpei <yoneyamashunpei@gmail.com>
2020-03-11 01:52:11 +0900

    add:dialog like iPhone

--- app/src/main/java/com/komeyama/sample/design/material/ui/dialog/DialogSelectFragment.kt
+++ app/src/main/java/com/komeyama/sample/design/material/ui/dialog/DialogSelectFragment.kt
@@ -3,6 +3,7 @@ package com.komeyama.sample.design.material.ui.dialog
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.komeyama.sample.design.material.R
 import kotlinx.android.synthetic.main.fragment_dialog_select.*
 
@@ -12,16 +13,12 @@ class DialogSelectFragment: Fragment(R.layout.fragment_dialog_select) {
         super.onViewCreated(view, savedInstanceState)
 
         dialog_type01.setOnClickListener {
-
+            findNavController().navigate(R.id.action_dialogSelectFragment_to_dialogType01)
         }
 
-        dialog_type02.setOnClickListener {
+        dialog_type02.setOnClickListener {}
 
-        }
-
-        dialog_type03.setOnClickListener {
-
-        }
+        dialog_type03.setOnClickListener {}
 
     }
 
