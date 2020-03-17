147e0c4c7f083a788c6d524428eb1624a2dc2280
YoneyamaShunpei <yoneyamashunpei@gmail.com>
2020-03-17 22:23:07 +0900

    update:card form of type03

--- app/src/main/java/com/komeyama/sample/design/material/ui/card/CardType03.kt
+++ app/src/main/java/com/komeyama/sample/design/material/ui/card/CardType03.kt
@@ -9,6 +9,7 @@ import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.databinding.ViewHolder
 import com.xwray.groupie.databinding.BindableItem
 import kotlinx.android.synthetic.main.fragment_card_type03.*
+import timber.log.Timber
 
 class CardType03: Fragment(R.layout.fragment_card_type03) {
 
@@ -18,21 +19,29 @@ class CardType03: Fragment(R.layout.fragment_card_type03) {
         val groupAdapter = GroupAdapter<ViewHolder<*>>()
         card_recycler_view.adapter = groupAdapter
         val items = listOf(
-            TextItem("test1"),
-            TextItem("test2"),
-            TextItem("test3"),
-            TextItem("test4"),
-            TextItem("test5"),
-            TextItem("test6")
+            CardItem("Card Title1","sub text 1", View.VISIBLE),
+            CardItem("Card Title2","sub text 2", View.GONE),
+            CardItem("Card Title3","sub text 3", View.INVISIBLE),
+            CardItem("Card Title4","sub text 4", View.VISIBLE),
+            CardItem("Card Title5","sub text 5", View.VISIBLE),
+            CardItem("Card Title6","sub text 6", View.VISIBLE)
         )
         groupAdapter.update(items)
     }
 }
 
-class TextItem(val text: String) : BindableItem<ListItemCardBinding>() {
+class CardItem(private val titleText: String, private val subTitleText: String, private val button2Visibility: Int) : BindableItem<ListItemCardBinding>() {
     override fun getLayout() = R.layout.list_item_card
 
     override fun bind(viewBinding: ListItemCardBinding, position: Int) {
-        viewBinding.textView.text = text
+        viewBinding.cardTitleText.text = titleText
+        viewBinding.cardSecondaryText.text = subTitleText
+        viewBinding.cardAction02.visibility = button2Visibility
+        viewBinding.cardAction01.setOnClickListener {
+            Timber.d("on click button1 position:%s, title:%s ",position, viewBinding.cardTitleText.text)
+        }
+        viewBinding.cardAction02.setOnClickListener {
+            Timber.d("on click button2 position:%s, title:%s ",position, viewBinding.cardSecondaryText.text)
+        }
     }
 }
