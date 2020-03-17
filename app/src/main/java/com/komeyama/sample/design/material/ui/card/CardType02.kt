1f844488489278c705a9f4193571227a036d5a9d
YoneyamaShunpei <yoneyamashunpei@gmail.com>
2020-03-17 22:52:18 +0900

    update:insert cart type02 into type03

--- app/src/main/java/com/komeyama/sample/design/material/ui/card/CardType02.kt
+++ app/src/main/java/com/komeyama/sample/design/material/ui/card/CardType02.kt
@@ -1,20 +1,13 @@
 package com.komeyama.sample.design.material.ui.card
 
 import android.os.Bundle
-import android.provider.Contacts
 import android.view.View
-import android.view.animation.Animation
-import android.view.animation.ScaleAnimation
-import android.widget.ImageView
 import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import com.komeyama.sample.design.material.R
 import kotlinx.android.synthetic.main.fragment_card_type02.*
-import kotlinx.coroutines.*
 
 class CardType02: Fragment(R.layout.fragment_card_type02){
-
-    private val initialAnimationTime = 100L
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -25,36 +18,5 @@ class CardType02: Fragment(R.layout.fragment_card_type02){
                 card_favorite.startFavoriteAnimation(0.2f, R.drawable.ic_favorite_pink_24dp)
             }
         }
-    }
-
-    private fun ImageView.startFavoriteAnimation(scaleValue: Float, color: Int) {
-        val view = this
-        GlobalScope.launch(Dispatchers.Main) {
-            val shrink = GlobalScope.async(Dispatchers.Main) {
-                view.startAnimation(
-                    scaleAnimationAsCenter(
-                    1.0f, scaleValue, 1.0f,scaleValue, initialAnimationTime
-                    )
-                )
-                delay(initialAnimationTime)
-            }
-            shrink.await()
-
-            view.setImageResource(color)
-            view.startAnimation(
-                scaleAnimationAsCenter(
-                scaleValue, 1.0f, scaleValue,1.0f,250)
-            )
-        }
-    }
-
-    private fun scaleAnimationAsCenter(fromX: Float,toX: Float,fromY: Float,toY: Float, duration: Long): Animation {
-        val scaleAnimation = ScaleAnimation(
-            fromX, toX, fromY,toY, Animation.RELATIVE_TO_SELF,
-            0.5f, Animation.RELATIVE_TO_SELF,0.5f)
-        scaleAnimation.duration = duration
-        scaleAnimation.repeatCount = 0
-        scaleAnimation.fillAfter = true
-        return scaleAnimation
     }
 }
--- app/src/main/java/com/komeyama/sample/design/material/ui/card/CardType03.kt
+++ app/src/main/java/com/komeyama/sample/design/material/ui/card/CardType03.kt
@@ -1,10 +1,13 @@
 package com.komeyama.sample.design.material.ui.card
 
+import android.content.res.Resources
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import com.komeyama.sample.design.material.R
-import com.komeyama.sample.design.material.databinding.ListItemCardBinding
+import com.komeyama.sample.design.material.databinding.ListItemCardType01Binding
+import com.komeyama.sample.design.material.databinding.ListItemCardType02Binding
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.databinding.ViewHolder
 import com.xwray.groupie.databinding.BindableItem
@@ -19,29 +22,53 @@ class CardType03: Fragment(R.layout.fragment_card_type03) {
         val groupAdapter = GroupAdapter<ViewHolder<*>>()
         card_recycler_view.adapter = groupAdapter
         val items = listOf(
-            CardItem("Card Title1","sub text 1", View.VISIBLE),
-            CardItem("Card Title2","sub text 2", View.GONE),
-            CardItem("Card Title3","sub text 3", View.INVISIBLE),
-            CardItem("Card Title4","sub text 4", View.VISIBLE),
-            CardItem("Card Title5","sub text 5", View.VISIBLE),
-            CardItem("Card Title6","sub text 6", View.VISIBLE)
+            CardType01Item("Card Title1","sub text 1", View.VISIBLE),
+            CardType01Item("Card Title2","sub text 2", View.GONE),
+            CardType01Item("Card Title3","sub text 3", View.INVISIBLE),
+            CardType01Item("Card Title4","sub text 4", View.VISIBLE),
+            CardType02Item("Card Title5","sub text 5", resources),
+            CardType01Item("Card Title6","sub text 6", View.VISIBLE)
         )
         groupAdapter.update(items)
     }
 }
 
-class CardItem(private val titleText: String, private val subTitleText: String, private val button2Visibility: Int) : BindableItem<ListItemCardBinding>() {
-    override fun getLayout() = R.layout.list_item_card
+class CardType01Item(private val titleText: String, private val subTitleText: String, private val button2Visibility: Int) : BindableItem<ListItemCardType01Binding>() {
+    override fun getLayout() = R.layout.list_item_card_type01
 
-    override fun bind(viewBinding: ListItemCardBinding, position: Int) {
+    override fun bind(viewBinding: ListItemCardType01Binding, position: Int) {
         viewBinding.cardTitleText.text = titleText
         viewBinding.cardSecondaryText.text = subTitleText
         viewBinding.cardAction02.visibility = button2Visibility
         viewBinding.cardAction01.setOnClickListener {
-            Timber.d("on click button1 position:%s, title:%s ",position, viewBinding.cardTitleText.text)
+            Timber.d("on click button1(type01) position:%s, title:%s ",position, viewBinding.cardTitleText.text)
         }
         viewBinding.cardAction02.setOnClickListener {
-            Timber.d("on click button2 position:%s, title:%s ",position, viewBinding.cardSecondaryText.text)
+            Timber.d("on click button2(type02) position:%s, title:%s ",position, viewBinding.cardSecondaryText.text)
+        }
+    }
+}
+
+class CardType02Item(private val titleText: String, private val subTitleText: String, private val resources: Resources) : BindableItem<ListItemCardType02Binding>() {
+
+    override fun getLayout() = R.layout.list_item_card_type02
+
+    override fun bind(viewBinding: ListItemCardType02Binding, position: Int) {
+        viewBinding.cardFavorite.setOnClickListener {
+            if (viewBinding.cardFavorite.drawable.constantState == ResourcesCompat.getDrawable(resources, R.drawable.ic_favorite_pink_24dp, null)?.constantState) {
+                viewBinding.cardFavorite.startFavoriteAnimation(1.2f, R.drawable.ic_favorite_24dp)
+            } else {
+                viewBinding.cardFavorite.startFavoriteAnimation(0.2f, R.drawable.ic_favorite_pink_24dp)
+            }
+        }
+
+        viewBinding.headerTitleText.text = titleText
+        viewBinding.subheadTitleText.text = subTitleText
+        viewBinding.cardAction01.setOnClickListener {
+            Timber.d("on click button1(type02) position:%s, title:%s ",position, viewBinding.headerTitleText.text)
+        }
+        viewBinding.cardAction02.setOnClickListener {
+            Timber.d("on click button2(type02) position:%s, title:%s ",position, viewBinding.subheadTitleText.text)
         }
     }
 }
