03b152a813ffec3ebcdf48ca2b94caec88b7f488
YoneyamaShunpei <yoneyamashunpei@gmail.com>
2020-03-19 23:02:10 +0900

    add:expand card

--- app/src/main/java/com/komeyama/sample/design/material/ui/card/CardType04.kt
+++ app/src/main/java/com/komeyama/sample/design/material/ui/card/CardType04.kt
@@ -1,8 +1,25 @@
 package com.komeyama.sample.design.material.ui.card
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.komeyama.sample.design.material.R
+import kotlinx.android.synthetic.main.fragment_card_type04.*
 
 class CardType04: Fragment(R.layout.fragment_card_type04) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        expand_control_button.setOnClickListener {
+            if (text_filed_constraint.visibility == View.GONE) {
+                card_top.layoutParams.height = activity?.resources?.getDimensionPixelSize(R.dimen.card_top_height_expand)!!.toInt()
+                text_filed_constraint.visibility = View.VISIBLE
+            } else {
+                card_top.layoutParams.height = activity?.resources?.getDimensionPixelSize(R.dimen.card_top_height_reduce)!!.toInt()
+                text_filed_constraint.visibility = View.GONE
+            }
+
+        }
+    }
 
 }
