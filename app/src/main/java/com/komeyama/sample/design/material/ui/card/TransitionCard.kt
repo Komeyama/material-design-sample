78354ddc43a79a0983427d197060c015565e0378
YoneyamaShunpei <yoneyamashunpei@gmail.com>
2020-03-29 22:41:55 +0900

    add: transition fragment

---
+++ app/src/main/java/com/komeyama/sample/design/material/ui/card/TransitionCard.kt
@@ -0,0 +1,6 @@
+package com.komeyama.sample.design.material.ui.card
+
+import androidx.fragment.app.Fragment
+import com.komeyama.sample.design.material.R
+
+class TransitionCard: Fragment(R.layout.fragment_transition_card) {}
