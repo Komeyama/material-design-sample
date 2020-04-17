5bcaad007202c9a76408ea988cd34f617db203fb
YoneyamaShunpei <yoneyamashunpei@gmail.com>
2020-04-17 22:51:22 +0900

    create card module

---
+++ feature/card/src/main/java/com/komeyama/sample/design/material/card/TransitionCard.kt
@@ -0,0 +1,28 @@
+package com.komeyama.sample.design.material.card
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.google.android.material.transition.MaterialContainerTransform
+import kotlinx.android.synthetic.main.fragment_transition_card.*
+
+class TransitionCard: Fragment(R.layout.fragment_transition_card) {
+
+    private val args: TransitionCardArgs by navArgs()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        sharedElementEnterTransition = MaterialContainerTransform(requireContext()).apply {
+            //duration = 3000L
+            //isDrawDebugEnabled = true
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        card_title_text.text =  args.titleName
+        view.rootView.transitionName = "transition_card_container"
+    }
+
+}
