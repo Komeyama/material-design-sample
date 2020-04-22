d56172515882e10999b74a2d8c845092c411a7ee
YoneyamaShunpei <yoneyamashunpei@gmail.com>
2020-04-22 23:12:14 +0900

    update: add validation of text field

--- feature/textfield/src/main/java/com/komeyama/sample/design/material/ui/textfield/TextFieldType01.kt
+++ feature/textfield/src/main/java/com/komeyama/sample/design/material/ui/textfield/TextFieldType01.kt
@@ -4,8 +4,13 @@ import android.os.Bundle
 import android.view.MotionEvent
 import android.view.View
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_text_field_type01.*
 
-class TextFieldType01: Fragment(R.layout.fragment_text_field_type01) {
+class TextFieldType01 : Fragment(R.layout.fragment_text_field_type01) {
+
+    companion object {
+        private const val EDIT_TEXT_PASSWORD_OUTLINE = 8
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -17,5 +22,24 @@ class TextFieldType01: Fragment(R.layout.fragment_text_field_type01) {
             }
             true
         }
+
+        edit_text_password_outline.validateOfEditTextField(
+            text_input_password_outline,
+            "Password must contain at least %s characters.",
+            EDIT_TEXT_PASSWORD_OUTLINE,
+            ValidateType.LESS_THAN
+        )
+        edit_text_password_filled.validateOfEditTextField(
+            text_input_password_filled,
+            "Password must contain at least %s characters.",
+            EDIT_TEXT_PASSWORD_OUTLINE,
+            ValidateType.LESS_THAN
+        )
+        edit_text_account_name_filled.validateOfEditTextField(
+            text_input_account_name_filled,
+            "Account name must be less than %s characters.",
+            text_input_account_name_filled.counterMaxLength,
+            ValidateType.MORE_THAN
+        )
     }
 }
--- feature/textfield/src/main/java/com/komeyama/sample/design/material/ui/textfield/Utils.kt
+++ feature/textfield/src/main/java/com/komeyama/sample/design/material/ui/textfield/Utils.kt
@@ -1,10 +1,53 @@
 package com.komeyama.sample.design.material.ui.textfield
 
 import android.content.Context
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+
+enum class ValidateType{
+    MORE_THAN,
+    LESS_THAN
+}
 
 fun View.hideKeyboard(view: View) {
     val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     imm.hideSoftInputFromWindow(view.windowToken, 0)
+}
+
+fun TextInputEditText.validateOfEditTextField(
+    textInputLayout: TextInputLayout,
+    errorMessage: String,
+    validateLength: Int,
+    validateType: ValidateType
+) {
+    val inputEditText = this
+
+    inputEditText.addTextChangedListener(object : TextWatcher {
+        override fun afterTextChanged(s: Editable?) {}
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            when(validateType) {
+                ValidateType.LESS_THAN -> {
+                    if (inputEditText.text!!.length < validateLength) {
+                        textInputLayout.error = String.format(errorMessage, validateLength)
+                    } else {
+                        textInputLayout.error = null
+                    }
+                }
+                ValidateType.MORE_THAN -> {
+                    if (inputEditText.text!!.length > validateLength) {
+                        textInputLayout.error = String.format(errorMessage, validateLength)
+                    } else {
+                        textInputLayout.error = null
+                    }
+                }
+            }
+        }
+    })
 }
