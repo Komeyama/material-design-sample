9f1da70c856cc9167b12da6fb5b882e132075fd0
YoneyamaShunpei <yoneyamashunpei@gmail.com>
2020-04-26 22:32:58 +0900

    add: prefix and suffix style

--- feature/textfield/src/main/java/com/komeyama/sample/design/material/ui/textfield/TextFieldSelectFragment.kt
+++ feature/textfield/src/main/java/com/komeyama/sample/design/material/ui/textfield/TextFieldSelectFragment.kt
@@ -11,10 +11,13 @@ class TextFieldSelectFragment: Fragment(R.layout.fragment_text_filed_select) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         text_type01_button.setOnClickListener {
-            findNavController().navigate(R.id.action_floatingSelectFragment_to_textfieldType01Fragment)
+            findNavController().navigate(R.id.action_textfieldSelectFragment_to_textfieldType01Fragment)
         }
         text_type02_button.setOnClickListener {
-            findNavController().navigate(R.id.action_floatingSelectFragment_to_textfieldType02Fragment)
+            findNavController().navigate(R.id.action_textfieldSelectFragment_to_textfieldType02Fragment)
+        }
+        text_type03_button.setOnClickListener {
+            findNavController().navigate(R.id.action_textfieldSelectFragment_to_textfieldType03Fragment)
         }
     }
 }
---
+++ feature/textfield/src/main/java/com/komeyama/sample/design/material/ui/textfield/TextFieldType03.kt
@@ -0,0 +1,6 @@
+package com.komeyama.sample.design.material.ui.textfield
+
+import androidx.fragment.app.Fragment
+
+class TextFieldType03: Fragment(R.layout.fragment_text_field_type03) {
+}
