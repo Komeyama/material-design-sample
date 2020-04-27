38c934e3088e3471d4402ba13bae99ee3b625481
YoneyamaShunpei <yoneyamashunpei@gmail.com>
2020-04-27 22:40:52 +0900

    add: selection control type01

--- feature/selectioncontrol/src/main/java/com/komeyama/sample/design/material/ui/selectioncontrol/SelectionControlSelectFragment.kt
+++ feature/selectioncontrol/src/main/java/com/komeyama/sample/design/material/ui/selectioncontrol/SelectionControlSelectFragment.kt
@@ -1,6 +1,17 @@
 package com.komeyama.sample.design.material.ui.selectioncontrol
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_selection_control_select.*
 
 class SelectionControlSelectFragment: Fragment(R.layout.fragment_selection_control_select) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        selection_control_type01.setOnClickListener {
+            findNavController().navigate(R.id.action_selectionControlSelectionFragment_to_selectionControlType01Fragment)
+        }
+    }
 }
