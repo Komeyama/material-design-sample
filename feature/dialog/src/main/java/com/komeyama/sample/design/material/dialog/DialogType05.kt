430ffbfe17e5773757015f2209c16a1d8d63a395
YoneyamaShunpei <yoneyamashunpei@gmail.com>
2020-05-06 19:06:55 +0900

    update: processing of  setting radio item on scrollable dialog

--- feature/dialog/src/main/java/com/komeyama/sample/design/material/dialog/DialogType05.kt
+++ feature/dialog/src/main/java/com/komeyama/sample/design/material/dialog/DialogType05.kt
@@ -2,7 +2,11 @@ package com.komeyama.sample.design.material.dialog
 
 import android.app.Dialog
 import android.os.Bundle
+import android.view.View
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.DialogFragment
 
 class DialogType05 : DialogFragment(){
@@ -12,6 +16,7 @@ class DialogType05 : DialogFragment(){
         val builder = AlertDialog.Builder(activity!!)
         val dialog = activity?.let {
             val view = it.layoutInflater.inflate(R.layout.fragment_dialog_type05,null)
+            setRadioItems(view)
             builder
                 .setTitle(resources.getString(R.string.dialog_title))
                 .setNegativeButton(resources.getString(R.string.dialog_cancel)) { _, _ -> }
@@ -24,5 +29,23 @@ class DialogType05 : DialogFragment(){
         return dialog as Dialog
     }
 
+    private fun setRadioItems(v: View) {
+        val radioGroup: RadioGroup = v!!.findViewById(R.id.selection_action_radio_group)
+        val itemList: List<String> = listOf("item1", "item2", "item3", "item4", "item5", "item6", "item7")
+        itemList.forEachIndexed { index, item ->
+            val radioButton = RadioButton(context)
+            radioButton.text = item
+            radioButton.buttonTintList = ContextCompat.getColorStateList(activity!!, R.color.colorPrimary)
+            radioButton.setPadding(
+                activity?.resources?.getDimensionPixelSize(R.dimen.dialog_type05_padding_start)!!.toInt(),
+                activity?.resources?.getDimensionPixelSize(R.dimen.dialog_type05_padding_row)!!.toInt(),
+                0,
+                activity?.resources?.getDimensionPixelSize(R.dimen.dialog_type05_padding_row)!!.toInt())
+            radioGroup.addView(radioButton)
+            if(index == 0){
+                radioButton.isChecked = true
+            }
+        }
+    }
 
 }
