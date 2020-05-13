4c4fe14135bf586eeac42ed194f30e63973725e0
YoneyamaShunpei <yoneyamashunpei@gmail.com>
2020-05-13 21:18:55 +0900

    add spinner date dialog to birthday text field

--- feature/textfield/src/main/java/com/komeyama/sample/design/material/ui/textfield/TextFieldType02.kt
+++ feature/textfield/src/main/java/com/komeyama/sample/design/material/ui/textfield/TextFieldType02.kt
@@ -1,10 +1,14 @@
 package com.komeyama.sample.design.material.ui.textfield
 
+import android.app.AlertDialog
+import android.app.Dialog
 import android.os.Bundle
 import android.view.View
 import android.widget.ArrayAdapter
+import android.widget.DatePicker
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_text_field_type02.*
+import timber.log.Timber
 
 class TextFieldType02 : Fragment(R.layout.fragment_text_field_type02) {
 
@@ -27,5 +31,27 @@ class TextFieldType02 : Fragment(R.layout.fragment_text_field_type02) {
             state
         )
         state_exposed_dropdown.setAdapter(stateAdapter)
+
+        edit_text_birthday.setOnClickListener {
+            Timber.d("okok")
+            showDataPickerDialog()
+        }
+    }
+
+    private fun showDataPickerDialog() {
+        val view = layoutInflater.inflate(R.layout.datepicker_dialog,null)
+        val datePickerDialog: Dialog = AlertDialog.Builder(context)
+            .setView(view)
+            .setTitle(resources.getString(R.string.dialog_title))
+            .setNegativeButton(resources.getString(R.string.dialog_cancel)) { _, _ -> }
+            .setPositiveButton(resources.getString(R.string.dialog_ok)) { _, _ -> }
+            .create()
+
+        val datePicker: DatePicker = view!!.findViewById(R.id.dialog_date_picker)
+        datePicker.init(2000, 1, 1) { _, year, month, day ->
+            Timber.d("date picker select date: %s %s %s", year, month + 1, day)
+        }
+
+        datePickerDialog.show()
     }
 }
