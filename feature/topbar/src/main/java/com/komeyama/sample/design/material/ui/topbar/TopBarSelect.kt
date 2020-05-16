26b29127258f4b83870aa9879e668085c57ce6be
YoneyamaShunpei <yoneyamashunpei@gmail.com>
2020-05-16 22:27:11 +0900

    add: type01 top appbar

--- feature/topbar/src/main/java/com/komeyama/sample/design/material/ui/topbar/TopBarSelect.kt
+++ feature/topbar/src/main/java/com/komeyama/sample/design/material/ui/topbar/TopBarSelect.kt
@@ -1,6 +1,17 @@
 package com.komeyama.sample.design.material.ui.topbar
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_top_bar_select.*
 
 class TopBarSelect: Fragment(R.layout.fragment_top_bar_select) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        top_bar_type01.setOnClickListener {
+            findNavController().navigate(R.id.action_mainFragment_to_topBarType01)
+        }
+    }
 }
---
+++ feature/topbar/src/main/java/com/komeyama/sample/design/material/ui/topbar/TopBarType01.kt
@@ -0,0 +1,6 @@
+package com.komeyama.sample.design.material.ui.topbar
+
+import androidx.fragment.app.Fragment
+
+class TopBarType01: Fragment(R.layout.fragment_top_bar_type01) {
+}
