a1db3b59af0c148c134eb1670d3955995fac2d1d
YoneyamaShunpei <yoneyamashunpei@gmail.com>
2020-05-26 23:22:50 +0900

    add: bottom navigation type01 view

--- feature/bottomnavigation/src/main/java/com/komeyama/sample/design/material/ui/bottomnavigation/BottomNavigationType01.kt
+++ feature/bottomnavigation/src/main/java/com/komeyama/sample/design/material/ui/bottomnavigation/BottomNavigationType01.kt
@@ -1,7 +1,13 @@
 package com.komeyama.sample.design.material.ui.bottomnavigation
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 
 class BottomNavigationType01 : Fragment(R.layout.fragment_bottom_navigation_type01){
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
 }
