75d4f5405a01e6458407525266cfabc934f3adbb
YoneyamaShunpei <yoneyamashunpei@gmail.com>
2020-05-26 22:56:37 +0900

    add: bottom navigation fragment type01

--- feature/bottomnavigation/src/main/java/com/komeyama/sample/design/material/ui/bottomnavigation/BottomNavigationSelect.kt
+++ feature/bottomnavigation/src/main/java/com/komeyama/sample/design/material/ui/bottomnavigation/BottomNavigationSelect.kt
@@ -1,6 +1,17 @@
 package com.komeyama.sample.design.material.ui.bottomnavigation
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_bottom_navigation_select.*
 
 class BottomNavigationSelect: Fragment(R.layout.fragment_bottom_navigation_select) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bottom_navigation_type01_button.setOnClickListener {
+            findNavController().navigate(R.id.action_bottomNavigationSelectFragment_to_bottomNavigationType01Fragment)
+        }
+    }
 }
---
+++ feature/bottomnavigation/src/main/java/com/komeyama/sample/design/material/ui/bottomnavigation/BottomNavigationType01.kt
@@ -0,0 +1,7 @@
+package com.komeyama.sample.design.material.ui.bottomnavigation
+
+import androidx.fragment.app.Fragment
+
+class BottomNavigationType01 : Fragment(R.layout.fragment_bottom_navigation_type01){
+
+}
