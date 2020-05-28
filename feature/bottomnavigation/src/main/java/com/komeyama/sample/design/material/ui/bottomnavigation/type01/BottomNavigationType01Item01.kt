f5cef36e9fbcffe056c14378a7ea5458461a4293
YoneyamaShunpei <yoneyamashunpei@gmail.com>
2020-05-28 20:57:20 +0900

    update: add list view to bottom nav type01 item01

--- feature/bottomnavigation/src/main/java/com/komeyama/sample/design/material/ui/bottomnavigation/type01/BottomNavigationType01Item01.kt
+++ feature/bottomnavigation/src/main/java/com/komeyama/sample/design/material/ui/bottomnavigation/type01/BottomNavigationType01Item01.kt
@@ -1,8 +1,42 @@
 package com.komeyama.sample.design.material.ui.bottomnavigation.type01
 
+import android.content.Context
+import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.ViewCompat
 import androidx.fragment.app.Fragment
 import com.komeyama.sample.design.material.ui.bottomnavigation.R
+import com.komeyama.sample.design.material.ui.bottomnavigation.databinding.BottomNavType01ItemBinding
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.databinding.BindableItem
+import com.xwray.groupie.databinding.ViewHolder
+import kotlinx.android.synthetic.main.fragment_bottom_navigation_type01_item01.*
+import timber.log.Timber
+import kotlin.math.max
+import kotlin.math.min
 
 class BottomNavigationType01Item01: Fragment(R.layout.fragment_bottom_navigation_type01_item01) {
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val groupAdapter = GroupAdapter<ViewHolder<*>>()
+        bottom_nav_type01_item01_recycler_view.adapter = groupAdapter
+        val items: MutableList<BottomNavType01Item> = mutableListOf()
+        for(index in 0 until 20) {
+            items.add(BottomNavType01Item("Bottom Navigation Item $index"))
+        }
+        groupAdapter.update(items)
+    }
+}
+
+class BottomNavType01Item(private val titleText: String) : BindableItem<BottomNavType01ItemBinding>() {
+    override fun getLayout() = R.layout.bottom_nav_type01_item
+
+    override fun bind(viewBinding: BottomNavType01ItemBinding, position: Int) {
+        viewBinding.topAppTitleText.text = titleText
+        Timber.d("on click button1(type01) position:%s, title:%s ",position, viewBinding.topAppTitleText.text)
+    }
 }
