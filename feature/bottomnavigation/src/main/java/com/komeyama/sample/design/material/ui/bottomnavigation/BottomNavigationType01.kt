6e188c5e07f996cbf3dcfe6b530c696c62827ee7
YoneyamaShunpei <yoneyamashunpei@gmail.com>
2020-05-29 22:02:15 +0900

    add: badge to bottom nav type01 view

--- feature/bottomnavigation/src/main/java/com/komeyama/sample/design/material/ui/bottomnavigation/BottomNavigationType01.kt
+++ feature/bottomnavigation/src/main/java/com/komeyama/sample/design/material/ui/bottomnavigation/BottomNavigationType01.kt
@@ -13,6 +13,16 @@ class BottomNavigationType01 : Fragment(R.layout.fragment_bottom_navigation_type
         super.onViewCreated(view, savedInstanceState)
         val navController = activity!!.findNavController(R.id.bottom_nav_view_type01_nav_host_fragment)
         NavigationUI.setupWithNavController(bottom_navigation_view_type01, navController)
+
+        val badge = bottom_navigation_view_type01.getOrCreateBadge(R.id.bottomNavigationType01FragmentItem02)
+        badge.isVisible = true
+        badge.number = 3
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            bottom_navigation_view_type01.removeBadge(destination.id)
+        }
     }
+
+
 
 }
