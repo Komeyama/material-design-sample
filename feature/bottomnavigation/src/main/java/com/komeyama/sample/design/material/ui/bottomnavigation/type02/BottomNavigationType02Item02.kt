4cb1f35225678166651f4479933386a3b065c14f
YoneyamaShunpei <yoneyamashunpei@gmail.com>
2020-06-02 21:56:32 +0900

    add tab layout on bottom nab type02

--- feature/bottomnavigation/src/main/java/com/komeyama/sample/design/material/ui/bottomnavigation/type02/BottomNavigationType02Item02.kt
+++ feature/bottomnavigation/src/main/java/com/komeyama/sample/design/material/ui/bottomnavigation/type02/BottomNavigationType02Item02.kt
@@ -1,7 +1,51 @@
 package com.komeyama.sample.design.material.ui.bottomnavigation.type02
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
 import com.komeyama.sample.design.material.ui.bottomnavigation.R
+import kotlinx.android.synthetic.main.fragment_bottom_navigation_type02_item02.*
 
 class BottomNavigationType02Item02: Fragment(R.layout.fragment_bottom_navigation_type02_item02) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bottom_navigation_type02_item02_pager.adapter = TabAdapter(activity!!.supportFragmentManager)
+        bottom_navigation_type02_item02_tabLayout.setupWithViewPager(bottom_navigation_type02_item02_pager)
+    }
 }
+
+class TabAdapter(fm: FragmentManager): FragmentPagerAdapter(fm){
+
+    override fun getItem(position: Int): Fragment {
+        return when(position){
+            0 -> {
+                BottomNavigationType02Item02Tab01()
+            }
+            else ->  {
+                BottomNavigationType02Item02Tab02()
+            }
+        }
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return when(position){
+            0 -> {
+                "tab_01"
+            }
+            else ->  {
+                "tab_02"
+            }
+        }
+    }
+
+    override fun getCount(): Int {
+        return 2
+    }
+}
+
+class BottomNavigationType02Item02Tab01: Fragment(R.layout.fragment_bottom_navigation_type02_item02_tab01){}
+
+class BottomNavigationType02Item02Tab02: Fragment(R.layout.fragment_bottom_navigation_type02_item02_tab02){}
