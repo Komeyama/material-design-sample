5524a6e8a472bf6a7087ae3aa6e73f59dc2bca85
YoneyamaShunpei <yoneyamashunpei@gmail.com>
2020-06-06 15:42:59 +0900

    update toolbar of album detail view

--- feature/bottomnavigation/src/main/java/com/komeyama/sample/design/material/ui/bottomnavigation/type02/item02/BottomNavigationType02Item02AlbumDetail.kt
+++ feature/bottomnavigation/src/main/java/com/komeyama/sample/design/material/ui/bottomnavigation/type02/item02/BottomNavigationType02Item02AlbumDetail.kt
@@ -1,17 +1,31 @@
 package com.komeyama.sample.design.material.ui.bottomnavigation.type02.item02
 
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
 import com.komeyama.sample.design.material.ui.bottomnavigation.R
-import timber.log.Timber
+import kotlinx.android.synthetic.main.fragment_bottom_navigation_type02_item02_album_detail.*
 
-class BottomNavigationType02Item02AlbumDetail: Fragment(R.layout.fragment_bottom_navigation_type02_item02_album_detail) {
+class BottomNavigationType02Item02AlbumDetail :
+    Fragment(R.layout.fragment_bottom_navigation_type02_item02_album_detail) {
+
     private val args: BottomNavigationType02Item02AlbumDetailArgs by navArgs()
+    private val marqueeStartTime: Long = 2000L
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Timber.d("args!!: %s %s %s", args.albumName, args.artistName, args.albumTime)
+
+        // set title text
+        val title = args.albumName + " - " + args.artistName
+        album_detail_toolbar_title.text = title
+        val handler = Handler()
+        handler.postDelayed({ album_detail_toolbar_title.isSelected = true }, marqueeStartTime)
+        album_detail_toolbar.setNavigationOnClickListener {
+            it.findNavController().navigateUp()
+        }
+
     }
 }
