ff310fd32bc5226fae60cf20a81da921e643bea0
YoneyamaShunpei <yoneyamashunpei@gmail.com>
2020-06-12 20:37:51 +0900

    add type01 button

--- feature/button/src/main/java/com/komeyama/sample/design/material/ui/button/ButtonSelect.kt
+++ feature/button/src/main/java/com/komeyama/sample/design/material/ui/button/ButtonSelect.kt
@@ -1,6 +1,17 @@
 package com.komeyama.sample.design.material.ui.button
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_button_select.*
 
 class ButtonSelect : Fragment(R.layout.fragment_button_select) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        button_select_type01.setOnClickListener {
+            findNavController().navigate(R.id.action_buttonSelectFragment_to_buttonType01Fragment)
+        }
+    }
 }
---
+++ feature/button/src/main/java/com/komeyama/sample/design/material/ui/button/ButtonType01.kt
@@ -0,0 +1,6 @@
+package com.komeyama.sample.design.material.ui.button
+
+import androidx.fragment.app.Fragment
+
+class ButtonType01 : Fragment(R.layout.fragment_button_type01) {
+}
