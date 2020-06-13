c191504da594abc1c0cb18cabdbc802de0dc49aa
YoneyamaShunpei <yoneyamashunpei@gmail.com>
2020-06-13 22:05:28 +0900

    add single select toggle button

--- feature/button/src/main/java/com/komeyama/sample/design/material/ui/button/ButtonType02.kt
+++ feature/button/src/main/java/com/komeyama/sample/design/material/ui/button/ButtonType02.kt
@@ -10,8 +10,12 @@ import timber.log.Timber
 class ButtonType02 : Fragment(R.layout.fragment_button_type02) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        button_type02_toggle.addOnButtonCheckedListener { toggleButton, checkedId, isChecked ->
-            Timber.d("toggle !: %s %s %s", toggleButton.checkedButtonIds, checkedId, isChecked)
+        button_type02_toggle_01.addOnButtonCheckedListener { toggleButton, checkedId, isChecked ->
+            Timber.d("toggle01: %s %s %s", toggleButton.checkedButtonIds, checkedId, isChecked)
+            Snackbar.make(view, "button$checkedId : $isChecked", Snackbar.LENGTH_LONG).show()
+        }
+        button_type02_toggle_02.addOnButtonCheckedListener { toggleButton, checkedId, isChecked ->
+            Timber.d("toggle02: %s %s %s", toggleButton.checkedButtonIds, checkedId, isChecked)
             Snackbar.make(view, "button$checkedId : $isChecked", Snackbar.LENGTH_LONG).show()
         }
 
