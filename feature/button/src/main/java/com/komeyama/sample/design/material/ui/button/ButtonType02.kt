82cc5ba817e5f6dd2100c138b813b57541ca0de7
YoneyamaShunpei <yoneyamashunpei@gmail.com>
2020-06-13 21:36:42 +0900

    add snackbar

--- feature/button/src/main/java/com/komeyama/sample/design/material/ui/button/ButtonType02.kt
+++ feature/button/src/main/java/com/komeyama/sample/design/material/ui/button/ButtonType02.kt
@@ -1,5 +1,19 @@
 package com.komeyama.sample.design.material.ui.button
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.fragment_button_type02.*
+import timber.log.Timber
 
-class ButtonType02 : Fragment(R.layout.fragment_button_type02) {}
+class ButtonType02 : Fragment(R.layout.fragment_button_type02) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        button_type02_toggle.addOnButtonCheckedListener { toggleButton, checkedId, isChecked ->
+            Timber.d("toggle !: %s %s %s", toggleButton.checkedButtonIds, checkedId, isChecked)
+            Snackbar.make(view, "button$checkedId : $isChecked", Snackbar.LENGTH_LONG).show()
+        }
+
+    }
+}
